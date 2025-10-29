c67c1cf51628219f491597aa74392a66527143ae
Alvaro Gonzalez <alvaro20dam@hotmail.com>
2025-10-29 12:40:47 -0400

    changes varios

--- airbnb-dashboard/data_helpers.py
+++ airbnb-dashboard/data_helpers.py
@@ -1,6 +1,5 @@
-import pandas as pd
+import pandas as pd 
 
-# Showing functions that you could augment to load data from a database or API
 
 def load_listings():
     return pd.read_csv('listings.csv')
